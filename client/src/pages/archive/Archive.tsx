8e257f02ac471110543bea8036d6a7b98010f266
tacss <tacss.co@gmail.com>
2025-09-12 11:19:54 +0530

    Revert "pdf"

--- client/src/pages/archive/Archive.tsx
+++ client/src/pages/archive/Archive.tsx
@@ -8,8 +8,6 @@ import { Link } from "react-router-dom";
 import { Viewer, Worker } from '@react-pdf-viewer/core';
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 
-
-
 // Import the Zoom plugin and its styles
 import { zoomPlugin } from '@react-pdf-viewer/zoom';
 import '@react-pdf-viewer/zoom/lib/styles/index.css';
@@ -107,7 +105,7 @@ export default function Archive() {
     const [loadingPdf, setLoadingPdf] = useState(false);
     // New state for the subscription popup
     const [showSubscriptionPopup, setShowSubscriptionPopup] = useState(false);
-
+    
     // Ref to store the AbortController for a cancellable request
     const abortControllerRef = useRef<AbortController | null>(null);
 
@@ -209,9 +207,9 @@ export default function Archive() {
     const getItemsForMonth = (year: number, month: number) => {
         return archiveMap[year]?.[month] || [];
     };
-
+    
     // Function to handle cancellation
-    const handleCancelRequest = () => {
+     const handleCancelRequest = () => {
         if (abortControllerRef.current) {
             abortControllerRef.current.abort();
             abortControllerRef.current = null;
@@ -395,7 +393,7 @@ export default function Archive() {
                             <MdClose className="text-2xl" />
                         </button>
                         <div className="flex-grow w-full overflow-auto">
-                            <Worker workerUrl="/pdf.worker.min.js">
+                            <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
                                 {activePdf.pdfUrl ? (
                                     <Viewer
                                         fileUrl={SERVER_URL + activePdf.pdfUrl}
